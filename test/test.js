c7d45c0e670c41f1a579812edc8ad9230e12364b
Dave Harned <dharned@gmail.com>
2016-08-04 11:24:52 -0500

    explore basic test structure

--- test/test.js
+++ test/test.js
@@ -1,7 +1,19 @@
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert; 
 
 describe ('A basic test', function() {
     it('should pass when test is set up properly', function() {
         expect(true).to.be.true;
+    })
+})
+
+// Describe what you're testing
+describe('Array', function() {
+    // It block - describe a specific behavior you're testing
+    it('should start empty', function() {
+        var arr = [];
+        
+        assert.equal(arr.length, 0);
     })
 })
