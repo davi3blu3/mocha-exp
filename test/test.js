6d421b91d7d91e1bbd505ed3c18e302acb6121ce
Dave Harned <dharned@gmail.com>
2016-08-04 12:50:14 -0500

    writing new tests for empty array

--- test/test.js
+++ test/test.js
@@ -1,19 +1,43 @@
+'use strict'
+
 var chai = require('chai');
 var expect = chai.expect;
-var assert = chai.assert; 
+var assert = chai.assert;
 
-describe ('A basic test', function() {
+describe('A basic test', function() {
     it('should pass when test is set up properly', function() {
         expect(true).to.be.true;
     })
-})
+});
 
 // Describe what you're testing
-describe('Array', function() {
+describe('Empty Array', function() {
+    // subject of testing
+    var arr = [];
+    
     // It block - describe a specific behavior you're testing
     it('should start empty', function() {
-        var arr = [];
-        
         assert.equal(arr.length, 0);
     })
-})
+    
+    it('should not be undefined', function() {
+        expect(typeof arr).to.not.be.undefined;
+    })
+    
+    it('should not be a null', function() {
+        expect(typeof arr).to.not.be.a('null');
+    })
+
+    it('should not be a number', function() {
+        expect(typeof arr).to.not.be.a('number');
+    })
+    
+    it('should not be a boolean', function() {
+        expect(typeof arr).to.not.be.a('boolean');
+    })
+    
+    it('first index should be undefined', function() {
+        expect(arr[0]).to.be.undefined;
+    })
+    
+});
