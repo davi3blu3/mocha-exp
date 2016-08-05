aae247bf57241596efc391ab931a1678348a6c24
Dave Harned <dharned@gmail.com>
2016-08-05 17:09:50 -0500

    five successful tests for leapYear

--- 1LeapYears.js
+++ 1LeapYears.js
@@ -1,13 +1,15 @@
-// Write a program that prints the next 20 leap years.
+// Write a program that returns the next 20 leap years.
 
 exports = module.exports = {};
 
 exports.leapYear = function() {
-//    var year = 2016,
-//        leaps = [];
-//    
-//    while (years.length <= 20) {
-//        
-//    }
-    return [1, 2, 3];
+    var year = 2016,
+        leaps = [];
+    
+    while (leaps.length < 20) {
+        leaps.push(year);
+        year += 4;
+    }
+    
+    return leaps;
 };
--- test/1LeapYearsTest.js
+++ test/1LeapYearsTest.js
@@ -7,13 +7,25 @@ var chai = require('chai'),
 var testFunc = require('../1LeapYears.js');
 
 describe('Leap Years', function() {
+    var func = testFunc.leapYear();
+    
     it ('should return an array', function() {
-        expect(testFunc.leapYear()).to.be.instanceof(Array);
+        expect(func).to.be.instanceof(Array);
     });
     
     it ('should include 20 years', function() {
-        expect(testFunc.leapYear().length).to.equal(20);
+        expect(func.length).to.equal(20);
     });
     
-    it ('first year should be 2016');//, function() {});
+    it ('first year should be 2016', function() {
+        expect(func[0]).to.equal(2016);
+    });
+    
+    it ('second year should be 2020', function() {
+        expect(func[1]).to.equal(2020);
+    });
+    
+    it ('last year should be 2092', function() {
+        expect(func[func.length-1]).to.equal(2092);
+    });
 });
