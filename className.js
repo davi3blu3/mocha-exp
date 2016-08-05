657c54cccda63c5541965afebca8042be0775577
Dave Harned <dharned@gmail.com>
2016-08-05 16:23:56 -0500

    successful export className to test file

--- className.js
+++ className.js
@@ -1,4 +1,6 @@
-function addClass(el, newClass) {
+var exports = module.exports = {};
+
+exports.addClass = function(el, newClass) {
   if(el.className.indexOf(newClass) === -1) {
     el.className += newClass;
   }
--- test/classNameTest.js
+++ test/classNameTest.js
@@ -1,10 +1,15 @@
+var chai = require('chai');
+var expect = chai.expect;
+var assert = chai.assert;
+var imports = require('../className.js');
+
 describe('addClass', function() {
     it('should add class to element', function() {
         var element = { className: ''};
         
-        addClass(element, 'test-class');
+        imports.addClass(element, 'test-class');
         
         assert.equal(element.className, 'test-class');
     });
-    it('should not add a class which already exists');
+
 });
